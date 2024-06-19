1a1d848abb3d0b5d6e51ec5f84d3c98f6fb79efd
Guany <1084072690@qq.com>
2024-06-19 11:03:04 +0800

    Update viewport meta tag for better mobile responsiveness

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -110,32 +110,39 @@ function Index() {
         <NavBar />
       </header>
 
-      <main className="pb-[100px] pt-[60px]">
+      <main className="py-[60px]">
         <div
           className="prose max-w-none p-[20px] dark:prose-invert"
           dangerouslySetInnerHTML={{ __html: response }}
         />
+
+        <div className="safe-area-bottom"></div>
       </main>
 
-      <footer className="fixed bottom-0 left-0 z-[999] flex h-[100px] w-full flex-col items-center  bg-white p-[20px] shadow dark:bg-black">
-        <form className="flex w-full gap-2" onSubmit={handleSend}>
-          <textarea
-            className="inp flex-1 resize-none text-left"
-            value={prompt}
-            onChange={(event) => setPrompt(event.target.value)}
-            autoFocus
-            rows={1}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter' && !event.shiftKey) {
-                event.preventDefault()
-                handleSend(event)
-              }
-            }}
-          />
-          <button className="btn" disabled={disabled}>
-            发送
-          </button>
-        </form>
+      <footer className="fixed bottom-0 left-0 z-[999] w-full bg-white px-[20px] shadow dark:bg-black">
+        <div className="flex h-[60px] items-center">
+          <form className="flex w-full gap-2" onSubmit={handleSend}>
+            <textarea
+              className="inp flex-1 resize-none text-left"
+              value={prompt}
+              onChange={(event) => setPrompt(event.target.value)}
+              autoFocus
+              rows={1}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter' && !event.shiftKey) {
+                  event.preventDefault()
+                  handleSend(event)
+                }
+              }}
+            />
+
+            <button className="btn" disabled={disabled}>
+              发送
+            </button>
+          </form>
+        </div>
+
+        <div className="safe-area-bottom"></div>
       </footer>
     </section>
   )
